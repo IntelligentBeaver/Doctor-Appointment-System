3132cac947f522011d3e9e4ac6e018d0f3a24ac2
SheikhAman <abamsheikh@gmail.com>
2023-12-22 22:16:08 +0545

    added comments to routes file

--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,27 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// HOMEPAGE Route
 Route::get('/',[HomeController::class,'view_Homepage'])->name('home');
 
+
+
+// Register Page Routes
+
+// This will show register.blade.php when somebody routes to name 'register'
+//Calls the view_Register() function to view register.blade.php (Function in app\Http\Controllers\AuthController.php)
 Route::get('/register', [AuthController::class, 'view_Register'])->name('register');
+
+// This takes all form data with POST Method
 Route::post('/registersuccess', [AuthController::class, 'register'])->name('registersuccess');
 
+
+
+//Login Page Routes
+
+// This will show login.blade.php when somebody routes to name 'login'
+//Calls the view_Login() function to view login.blade.php (Function in app\Http\Controllers\AuthController.php)
 Route::get('/login', [AuthController::class, 'view_Login'])->name('login');
+
+// This takes all form data with POST Method
 Route::post('/loginsuccess', [AuthController::class, 'login'])->name('loginsuccess');
