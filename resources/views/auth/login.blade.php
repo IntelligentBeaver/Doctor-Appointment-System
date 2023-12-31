43327133fbea36f06ef114e4d1b3f60751ac02a3
SheikhAman <abamsheikh@gmail.com>
2023-12-31 16:15:56 +0545

    Login and Register Page Updates

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,42 +1,79 @@
 <x-signinlogin>
 
-    <x-styling.header>
-        Login
-    </x-styling.header>
     <!-- Session Status -->
     @if (session('status'))
         <p>{{ session('status') }}</p>
     @endif
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
 
-        <!-- Email Address -->
-        <div>
-            <label for="email">Email:</label>
-            <input id="email" name="email" type="email" value="{{ old('email') }}" required autofocus
-                autocomplete="username">
-        </div>
+    <div class="bg-base-200 my-50 mx-auto my-20 max-w-[500px] rounded-xl px-10 py-14">
+        <h1 class="text-5xl font-bold">
+            Login
+        </h1>
+        <form method="POST" action="{{ route('login') }}">
+            @csrf
+            <div class="py-2">
+                <label class="form-control w-full">
+                    <div class="label">
+                        <span class="label-text text-base">Email:</span>
+                    </div>
+                    <input class="rounded-xl" id="email" name="email" type="email" value="{{ old('email') }}"
+                        required autofocus autocomplete="username">
+                </label>
+            </div>
 
-        <!-- Password -->
-        <div>
-            <label for="password">Password:</label>
-            <input id="password" name="password" type="password" required autocomplete="current-password">
-        </div>
 
-        <!-- Remember Me -->
-        <div>
-            <label>
-                <input id="remember_me" name="remember" type="checkbox">
-                Remember me
-            </label>
-        </div>
+            <div class="py-2">
+                <label class="form-control w-full">
+                    <div class="label">
+                        <span class="label-text text-base">Password:</span>
+                    </div>
+                    <input class="rounded-xl" id="password" name="password" type="password" required
+                        autocomplete="current-password">
+                </label>
+            </div>
 
-        <div>
-            @if (Route::has('password.request'))
-                <a href="{{ route('password.request') }}">Forgot your password?</a>
-            @endif
 
-            <button type="submit">Log in</button>
-        </div>
-    </form>
+            <div class="py-2">
+                <p>Don't have an account? <a href="{{ route('register') }}">Sign in</a></p>
+            </div>
+
+            <div class="py-2">
+                <button class="btn btn-secondary" type=" submit">Login</button>
+            </div>
+
+
+
+
+            <!-- Email Address -->
+            {{-- <div>
+                <label for="email">Email:</label>
+                <input id="email" name="email" type="email" value="{{ old('email') }}" required autofocus
+                    autocomplete="username">
+            </div> --}}
+
+
+            <!-- Password -->
+            {{-- <div>
+                <label for="password">Password:</label>
+                <input id="password" name="password" type="password" required autocomplete="current-password">
+            </div> --}}
+
+
+
+            <!-- Remember Me -->
+            {{-- <div>
+                <label>
+                    <input id="remember_me" name="remember" type="checkbox">
+                    Remember me
+                </label>
+            </div> --}}
+
+            {{-- <div>
+                @if (Route::has('password.request'))
+                    <a href="{{ route('password.request') }}">Forgot your password?</a>
+                @endif
+                <button type="submit">Log in</button>
+            </div> --}}
+        </form>
+    </div>
 </x-signinlogin>
